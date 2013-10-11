3ad31f3fdc10a796348bb1276920ddeca65caeb5
jayhutfles <jay@hutfles.com>
2013-10-11 08:58:02 -0500

    Update init.sh

--- ephemeral-hdfs/init.sh
+++ ephemeral-hdfs/init.sh
@@ -9,11 +9,11 @@ fi
 
 case "$HADOOP_MAJOR_VERSION" in
   1)
-    wget http://d3kbcqa49mib13.cloudfront.net/hadoop-1.0.4.tar.gz
+    wget http://mirror.reverse.net/pub/apache/hadoop/common/hadoop-1.2.1/hadoop-1.2.1-bin.tar.gz
     echo "Unpacking Hadoop"
-    tar xvzf hadoop-1.0.4.tar.gz > /tmp/spark-ec2_hadoop.log
+    tar xvzf hadoop-1.2.1.tar.gz > /tmp/spark-ec2_hadoop.log
     rm hadoop-*.tar.gz
-    mv hadoop-1.0.4/ ephemeral-hdfs/
+    mv hadoop-1.2.1/ ephemeral-hdfs/
     sed -i 's/-jvm server/-server/g' /root/ephemeral-hdfs/bin/hadoop
     ;;
   2) 
